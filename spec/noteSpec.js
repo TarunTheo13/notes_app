0ad7713fce68974fe705bec6e72152dc3800a26c
Masako Gray <masakogray@hotmail.co.uk>
2021-07-14 10:30:33 +0100

    adding 3rd test

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -7,4 +7,9 @@ it('creates a note', function(){
 it('shows the first 20 characters of the note', function(){
     let note = new Notes;
     expect(note.createNote('These are my favourite books')).toEqual(note.keys)
-  })
+});
+
+it('Saves the original note, not just 20 characters', function(){
+  let note = new Notes;
+  expect(note.createNote('Well if these notes are not saved, I will be gutted')).toEqual(note.value)
+});
