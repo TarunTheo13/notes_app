9b77d5e043b3bfd42cc74258b168fb9b678063b5
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-07-14 16:25:28 +0100

    showing created note abbreviations

--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   
+  const updateNotes = () => {
+    document.getElementById('writtenNotes').innerText = notes.seeListIntros();
+  }
+  
   const notes = new Notes();
 
   document.querySelector('#create').addEventListener('click', () => {
     notes.createNote(document.getElementById('pad').value);
     console.log(notes.notes)
     document.getElementById('pad').value = ""
+    updateNotes()
   });
 });
+
+
+// intros.forEach(element => {
+//   <href>element</href>
+// };
+
+//document.createElement(a)
--- src/notes.js
+++ src/notes.js
@@ -7,11 +7,19 @@ class Notes {
 
   createNote(note) {
     if (note.length <= 20) {
-      this.notes.push({[note]: note});
+      this.notes.push([note, note]);
     } else {
       let abbreviation = note.substring(0, 20)
-      this.notes.push({[abbreviation]: note});
+      this.notes.push([abbreviation, note]);
     };
+  };
+
+  seeListIntros() {
+    let intros = [];
+    for (let i = 0; i < this.notes.length; i++){
+      intros.push(this.notes[i][0]);
+    };
+    return intros;
   };
 
 };
