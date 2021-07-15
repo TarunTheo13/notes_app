ed5e77cfa87ee66ec3bb6081bbfed4628e5db9e7
Masako Gray <masakogray@hotmail.co.uk>
2021-07-16 00:48:26 +0100

    create all background, added fades

--- src/interface.js
+++ src/interface.js
@@ -6,14 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const updateNotes = () => {
     const array = notes.notes[noteCounter];
     const newA = document.createElement("a");
+    
     const newContent = document.createTextNode(array[0]);
     newA.appendChild(newContent);
+    newA.classList.add("listNotes");
     const currentElement = document.getElementById("pikachu");
     document.body.insertBefore(newA, currentElement);
     newA.href = ""
     newA.addEventListener("click", function(clickEvent) {
       clickEvent.preventDefault();
       document.getElementById("fullNote").innerHTML = array[1];
+      document.getElementById('fullNote').classList.add("fullNotes");
     });
     const newBreak = document.createElement("br");
     document.body.insertBefore(newBreak, currentElement);
@@ -23,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('#create').addEventListener('click', () => {
     notes.createNote(document.getElementById('pad').value);
     document.getElementById('pad').value = ""
-    updateNotes()
+    updateNotes();
   });
 
   const data = { username: 'example' };
