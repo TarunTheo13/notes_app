e54c98444d567e572ff479ee3bf336ae5274b1c3
Masako Gray <masakogray@hotmail.co.uk>
2021-07-16 14:40:16 +0100

    fade in fullnote output

--- src/interface.js
+++ src/interface.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const newContent = document.createTextNode(array[0]);
     newA.appendChild(newContent);
-    newA.classList.add("listNotes", "gradient_border");
+    newA.classList.add("listNotes");
     const currentElement = document.getElementById("pikachu");
     document.body.insertBefore(newA, currentElement);
     newA.href = ""
