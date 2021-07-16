dd5fbab9901fd4a0f0070fcaadcb4b1e82ce02ad
Masako Gray <masakogray@hotmail.co.uk>
2021-07-16 14:05:09 +0100

    refactored design

--- src/interface.js
+++ src/interface.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const newContent = document.createTextNode(array[0]);
     newA.appendChild(newContent);
-    newA.classList.add("listNotes");
+    newA.classList.add("listNotes", "gradient_border");
     const currentElement = document.getElementById("pikachu");
     document.body.insertBefore(newA, currentElement);
     newA.href = ""
